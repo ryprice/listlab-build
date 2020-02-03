7ecbb4e175c00a14b826a6b7e563f52fc08e981b
Ryan Price <ryanmichaelprice@gmail.com>
2020-02-02 20:46:34 -0500

    Fix local addresses with .local

--- config.local.js
+++ config.local.js
@@ -1,9 +1,9 @@
 var withApiConfig = window.listlabConfig;
-withApiConfig.TaskServiceAddress = 'http://api.listlab.io:8086/tasks';
-withApiConfig.AuthServiceAddress = 'http://api.listlab.io:8085/sts';
-withApiConfig.UserServiceAddress = 'http://api.listlab.io:8084/users';
-withApiConfig.NotificationServiceAddress = 'http://api.listlab.io:8083/notifications';
-withApiConfig.CommentServiceAddress = 'http://api.listlab.io:8081/comments';
-withApiConfig.ListServiceAddress = 'http://api.listlab.io:8082/lists';
+withApiConfig.TaskServiceAddress = 'http://api.listlab.local:8086/tasks';
+withApiConfig.AuthServiceAddress = 'http://api.listlab.local:8085/sts';
+withApiConfig.UserServiceAddress = 'http://api.listlab.local:8084/users';
+withApiConfig.NotificationServiceAddress = 'http://api.listlab.local:8083/notifications';
+withApiConfig.CommentServiceAddress = 'http://api.listlab.local:8081/comments';
+withApiConfig.ListServiceAddress = 'http://api.listlab.local:8082/lists';
 
 window.listlabConfig = withApiConfig;
