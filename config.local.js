d4ae4df66471972ab424083cc365cd66cf33b1cc
Ryan Price <ryanmichaelprice@gmail.com>
2020-10-23 22:58:09 -0400

    Update config for local.listlab.io

--- config.local.js
+++ config.local.js
@@ -1,10 +1,10 @@
 var withApiConfig = window.listlabConfig;
-withApiConfig.TaskServiceAddress = 'https://api.listlab.local:8080/tasks';
-withApiConfig.AuthServiceAddress = 'https://api.listlab.local:8080/sts';
-withApiConfig.UserServiceAddress = 'https://api.listlab.local:8080/users';
-withApiConfig.NotificationServiceAddress = 'https://api.listlab.local:8080/notifications';
-withApiConfig.CommentServiceAddress = 'https://api.listlab.local:8080/comments';
-withApiConfig.ListServiceAddress = 'https://api.listlab.local:8080/lists';
-withApiConfig.RootServiceAddress = 'https://api.listlab.local:8080';
+withApiConfig.TaskServiceAddress = 'https://api.local.listlab.io:8080/tasks';
+withApiConfig.AuthServiceAddress = 'https://api.local.listlab.io:8080/sts';
+withApiConfig.UserServiceAddress = 'https://api.local.listlab.io:8080/users';
+withApiConfig.NotificationServiceAddress = 'https://api.local.listlab.io:8080/notifications';
+withApiConfig.CommentServiceAddress = 'https://api.local.listlab.io:8080/comments';
+withApiConfig.ListServiceAddress = 'https://api.local.listlab.io:8080/lists';
+withApiConfig.RootServiceAddress = 'https://api.local.listlab.io:8080';
 
 window.listlabConfig = withApiConfig;
