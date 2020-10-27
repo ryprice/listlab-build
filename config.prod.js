0d45148c7425ab03d0bc7fbe4dc50cef0381869c
Ryan Price <ryanmichaelprice@gmail.com>
2020-10-27 02:35:48 -0400

    config.prod missing https on RootServiceAddress

--- config.prod.js
+++ config.prod.js
@@ -5,6 +5,6 @@ withApiConfig.UserServiceAddress = 'https://api.listlab.io/users';
 withApiConfig.NotificationServiceAddress = 'https://api.listlab.io/notifications';
 withApiConfig.CommentServiceAddress = 'https://api.listlab.io/comments';
 withApiConfig.ListServiceAddress = 'https://api.listlab.io/lists';
-withApiConfig.RootServiceAddress = 'http://api.listlab.io';
+withApiConfig.RootServiceAddress = 'https://api.listlab.io';
 
 window.listlabConfig = withApiConfig;
